daca4b8d7538989deac51015d59f69f0f2b56405
Sarfraz04 <sarfrazkist@gmail.com>
2023-11-29 13:43:02 +0530

    commit1

---
+++ src/test/java/day1/ProTest1.java
@@ -0,0 +1,10 @@
+package day1;
+
+import org.testng.annotations.Test;
+
+public class ProTest1 {
+@Test
+public void test1() throws Exception {
+	System.out.println("hiii    hii hii hii hii hii g");
+}
+}
