0bff216e367ffd9dfbced02904c11de9d44a3bd4
Sarfraz04 <sarfrazkist@gmail.com>
2023-11-30 12:31:02 +0530

    commit480

--- src/test/java/day1/Program2Test.java
+++ src/test/java/day1/Program2Test.java
@@ -10,11 +10,12 @@ import org.testng.annotations.Test;
 public class Program2Test {
 	@Test
 	public void test() {
-		WebDriver driver =new FirefoxDriver();
+		WebDriver driver =new ChromeDriver();
 		driver.manage().window().maximize();
 		System.out.println("open browser");
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		
 		System.out.println(driver.getTitle());
 		System.out.println("hi");
