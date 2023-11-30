b11770ce7447a64d0a8989b65b180df0489de246
Sarfraz04 <sarfrazkist@gmail.com>
2023-11-30 12:47:53 +0530

    commit543

---
+++ src/test/java/day1/LaunchedBrowserTest.java
@@ -0,0 +1,22 @@
+package day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchedBrowserTest {
+	WebDriver driver;
+	@Test
+	public void miTest() {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.quit();
+				
+		
+	}
+
+}
