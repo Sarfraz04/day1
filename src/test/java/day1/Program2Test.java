5665d842615876e7eb819dea917111c4cfd753ac
Sarfraz04 <sarfrazkist@gmail.com>
2023-11-30 10:10:05 +0530

    update

--- src/test/java/day1/Program2Test.java
+++ src/test/java/day1/Program2Test.java
@@ -4,16 +4,18 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Program2Test {
 	@Test
 	public void test() {
-		WebDriver driver =new ChromeDriver();
+		WebDriver driver =new FirefoxDriver();
 		driver.manage().window().maximize();
 		System.out.println("open browser");
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
 		System.out.println(driver.getTitle());
 		System.out.println("hi");
 		System.out.println("hi hello");
